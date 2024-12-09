fc684d8833f899c5cfa614fce37ae5d042616b58
Frientia <102570200+Frientia@users.noreply.github.com>
2024-12-10 05:40:55 +0700

    Update 2024_12_09_163405_kelas.php

--- database/migrations/2024_12_09_163405_kelas.php
+++ database/migrations/2024_12_09_163405_kelas.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('kelas', function (Blueprint $table) {
             $table->string('id_kelas', 10)->primary();
             $table->string('nama_kelas', 25);
+            $table->timestamps();
         });
     }
 

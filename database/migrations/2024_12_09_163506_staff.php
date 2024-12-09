226ccb629693130b185094568149d36dcc7102fd
Frientia <102570200+Frientia@users.noreply.github.com>
2024-12-10 05:31:14 +0700

    Update 2024_12_09_163506_staff.php

--- database/migrations/2024_12_09_163506_staff.php
+++ database/migrations/2024_12_09_163506_staff.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('staff', function (Blueprint $table) {
-            $table->integer('id_staff')->primary();
+            $table->string('id_staff', 10)->primary();
             $table->string('nama_staff', 25);
             $table->string('jabatan', 25);
             $table->text('no_telp');

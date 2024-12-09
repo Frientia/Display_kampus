76d7376dab6a6667bc7cdad5ffeeec1a43b5f009
Frientia <102570200+Frientia@users.noreply.github.com>
2024-12-10 05:43:14 +0700

    Update 2024_12_09_163417_jadwal.php

--- database/migrations/2024_12_09_163417_jadwal.php
+++ database/migrations/2024_12_09_163417_jadwal.php
@@ -9,15 +9,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('jadwal', function (Blueprint $table) {
-            $table->integer('id_jadwal', 10)->primary();
+            $table->string('id_jadwal', 10)->primary();
             $table->date('tanggal');
             $table->time('jam_masuk');
             $table->time('jam_selesai');
-            $table->integer('id_matkul')->unsigned();
-            $table->integer('id_dosen')->unsigned();
+            $table->string('id_matkul', 10);
+            $table->string('id_dosen', 10);
+            $table->string('id_ruangan', 10);
+            $table->string('id_kelas', 10);
             
-            $table->foreign('id_matkul')->references('id_matkul')->on('matkul');
-            $table->foreign('id_dosen')->references('id_dosen')->on('dosen');
+            $table->foreign('id_matkul')->references('id_matkul')->on('matkul')->onDelete('cascade');
+            $table->foreign('id_dosen')->references('id_dosen')->on('dosen')->onDelete('cascade');
+            $table->foreign('id_ruangan')->references('id_ruangan')->on('ruangan')->onDelete('cascade');
+            $table->foreign('id_kelas')->references('id_kelas')->on('kelas')->onDelete('cascade');
+            $table->timestamps();
         });
     }
 

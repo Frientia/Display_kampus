f6a4c12017e7eda7423bb79c05362a460e4c781d
Muhamad Yajid Rizky <102570200+Frientia@users.noreply.github.com>
2024-12-17 00:58:26 +0700

    membuat Migrasi

--- database/migrations/2024_12_10_162616_create_akuns_table.php.php
+++ database/migrations/2024_12_10_162616_create_akuns_table.php.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('akun', function (Blueprint $table) {
+        Schema::create('akuns', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique();
             $table->string('password');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('akun');
+        Schema::dropIfExists('akuns');
     }
 };

65d3e78bd460d096872a8aa30fdfb88906e74247
Frientia <muhamadyajidky.com>
2024-12-21 14:40:51 +0700

    Test Branch

--- database/migrations/2024_12_09_163402_matkul.php
+++ database/migrations/2024_12_09_163402_matkul.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('matkul', function (Blueprint $table) {
             $table->string('id_matkul', 10)->primary();
             $table->string('nama_matkul', 25);
-            $table->integer('sks');
+            $table->integer('sks', 20);
             $table->timestamps();
         });
     }
